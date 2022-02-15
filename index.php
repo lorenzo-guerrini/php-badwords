d317126f58a8e9fa5bb68837ab8c591620ad9016
lorenzo-guerrini <lorenzoguerrini8@live.it>
2022-02-15 09:43:29 +0100

    Parola passata tramite toGet

--- index.php
+++ index.php
@@ -10,9 +10,13 @@
 
     <?php
     $paragraph = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate a, quam excepturi adipisci, esse magnam molestiae doloribus nulla tempore odio laudantium corrupti, sit ullam libero. Dolor, reiciendis. Blanditiis quam dicta delectus reiciendis architecto, exercitationem, vero ipsa commodi, adipisci ipsam voluptatem modi dolorum eius enim! Illo reprehenderit alias, culpa adipisci nam beatae voluptatibus reiciendis aspernatur aperiam totam esse dolor quasi incidunt quam quibusdam! Animi, delectus inventore nihil doloribus at eos illo accusantium consectetur excepturi dignissimos nobis repudiandae perferendis impedit aut id porro optio? Ipsa esse odit porro eligendi earum, quaerat a minima quidem nihil aut dicta assumenda tempore similique. Quod, dolor!';
+
+    $toHide = $_GET["hide"];
     ?>
 
     <p> <?php echo $paragraph ?> </p>
     <div> Paragraph length <?php echo strlen($paragraph) ?> </div>
+    <div> <?php echo $toHide ?> </div>
+
 </body>
 </html>
