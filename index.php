fd9c6cd32f69a382a46c9dd7dece7588d6b5f86e
lorenzo-guerrini <lorenzoguerrini8@live.it>
2022-02-15 09:57:30 +0100

    Fine esercizio

--- index.php
+++ index.php
@@ -9,13 +9,15 @@
 <body>
 
     <?php
-    $paragraph = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate a, quam excepturi adipisci, esse magnam molestiae doloribus nulla tempore odio laudantium corrupti, sit ullam libero. Dolor, reiciendis. Blanditiis quam dicta delectus reiciendis architecto, exercitationem, vero ipsa commodi, adipisci ipsam voluptatem modi dolorum eius enim! Illo reprehenderit alias, culpa adipisci nam beatae voluptatibus reiciendis aspernatur aperiam totam esse dolor quasi incidunt quam quibusdam! Animi, delectus inventore nihil doloribus at eos illo accusantium consectetur excepturi dignissimos nobis repudiandae perferendis impedit aut id porro optio? Ipsa esse odit porro eligendi earum, quaerat a minima quidem nihil aut dicta assumenda tempore similique. Quod, dolor!';
+    $paragraphOG = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate a, quam excepturi adipisci, esse magnam molestiae doloribus nulla tempore odio laudantium corrupti, sit ullam libero. Dolor, reiciendis. Blanditiis quam dicta delectus reiciendis architecto, exercitationem, vero ipsa commodi, adipisci ipsam voluptatem modi dolorum eius enim! Illo reprehenderit alias, culpa adipisci nam beatae voluptatibus reiciendis aspernatur aperiam totam esse dolor quasi incidunt quam quibusdam! Animi, delectus inventore nihil doloribus at eos illo accusantium consectetur excepturi dignissimos nobis repudiandae perferendis impedit aut id porro optio? Ipsa esse odit porro eligendi earum, quaerat a minima quidem nihil aut dicta assumenda tempore similique. Quod, dolor!';
 
     $toHide = $_GET["hide"];
+
+    $paragraphCensored = str_replace($toHide, '***', $paragraphOG);
     ?>
 
-    <p> <?php echo $paragraph ?> </p>
-    <div> Paragraph length <?php echo strlen($paragraph) ?> </div>
+    <p> <?php echo $paragraphCensored ?> </p>
+    <div> Paragraph length <?php echo strlen($paragraphCensored) ?> </div>
     <div> <?php echo $toHide ?> </div>
 
 </body>
