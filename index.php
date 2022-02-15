5960e79bf7c376a955d25488109129437395ccd0
lorenzo-guerrini <lorenzoguerrini8@live.it>
2022-02-15 09:33:54 +0100

    Creata variabile con paragrafo

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Badwords</title>
+</head>
+<body>
+
+<?php
+$paragraph = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate a, quam excepturi adipisci, esse magnam molestiae doloribus nulla tempore odio laudantium corrupti, sit ullam libero. Dolor, reiciendis. Blanditiis quam dicta delectus reiciendis architecto, exercitationem, vero ipsa commodi, adipisci ipsam voluptatem modi dolorum eius enim! Illo reprehenderit alias, culpa adipisci nam beatae voluptatibus reiciendis aspernatur aperiam totam esse dolor quasi incidunt quam quibusdam! Animi, delectus inventore nihil doloribus at eos illo accusantium consectetur excepturi dignissimos nobis repudiandae perferendis impedit aut id porro optio? Ipsa esse odit porro eligendi earum, quaerat a minima quidem nihil aut dicta assumenda tempore similique. Quod, dolor!'
+?>
+
+</body>
+</html>
