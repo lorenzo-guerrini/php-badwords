5895d1ff72368b702d6951480f204ef1e336b55c
lorenzo-guerrini <lorenzoguerrini8@live.it>
2022-02-15 10:01:47 +0100

    Correzioni varie

--- index.php
+++ index.php
@@ -1,24 +1,26 @@
+<?php
+    $paragraphOG = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate a, quam excepturi adipisci, esse magnam molestiae doloribus nulla tempore odio laudantium corrupti, sit ullam libero. Dolor, reiciendis. Blanditiis quam dicta delectus reiciendis architecto, exercitationem, vero ipsa commodi, adipisci ipsam voluptatem modi dolorum eius enim! Illo reprehenderit alias, culpa adipisci nam beatae voluptatibus reiciendis aspernatur aperiam totam esse dolor quasi incidunt quam quibusdam! Animi, delectus inventore nihil doloribus at eos illo accusantium consectetur excepturi dignissimos nobis repudiandae perferendis impedit aut id porro optio? Ipsa esse odit porro eligendi earum, quaerat a minima quidem nihil aut dicta assumenda tempore similique. Quod, dolor!';
+
+    $wordToHide = $_GET["hide"];
+
+    $paragraphCensored = str_replace($wordToHide, '***', $paragraphOG);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Badwords</title>
 </head>
 <body>
 
-    <?php
-    $paragraphOG = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate a, quam excepturi adipisci, esse magnam molestiae doloribus nulla tempore odio laudantium corrupti, sit ullam libero. Dolor, reiciendis. Blanditiis quam dicta delectus reiciendis architecto, exercitationem, vero ipsa commodi, adipisci ipsam voluptatem modi dolorum eius enim! Illo reprehenderit alias, culpa adipisci nam beatae voluptatibus reiciendis aspernatur aperiam totam esse dolor quasi incidunt quam quibusdam! Animi, delectus inventore nihil doloribus at eos illo accusantium consectetur excepturi dignissimos nobis repudiandae perferendis impedit aut id porro optio? Ipsa esse odit porro eligendi earum, quaerat a minima quidem nihil aut dicta assumenda tempore similique. Quod, dolor!';
-
-    $toHide = $_GET["hide"];
-
-    $paragraphCensored = str_replace($toHide, '***', $paragraphOG);
-    ?>
-
     <p> <?php echo $paragraphCensored ?> </p>
-    <div> Paragraph length <?php echo strlen($paragraphCensored) ?> </div>
-    <div> <?php echo $toHide ?> </div>
+    
+    <div style="text-align: center">
+        <div> <strong>Paragraph length:</strong> <?php echo strlen($paragraphCensored) ?> </div>
+        <div> <strong>Censored word:</strong> <?php echo $wordToHide ?> </div>
+    </div>
 
 </body>
 </html>
